9e3ddf2c73dc4a23a2f377791cdfc2f66f2ed4ce
Canop <cano.petrole@gmail.com>
2021-09-20 21:08:55 +0200

    some status information displayed top right

--- src/tui/title_view.rs
+++ src/tui/title_view.rs
@@ -4,8 +4,8 @@ use {
     crossterm::{
         style::{Color, Color::*},
     },
-    minimad::{mad_inline, Alignment},
-    termimad::{ansi, gray, Area, MadSkin},
+    minimad::{Alignment, Composite},
+    termimad::{ansi, gray, Area, CompoundStyle, MadSkin},
 };
 
 pub struct TitleView {
@@ -35,10 +35,10 @@ impl View for TitleView {
 
     fn draw(&mut self, w: &mut W, state: &mut AppState) -> Result<(), SafeClosetError> {
         let path = state.open_closet.path().to_string_lossy();
-        let composite = mad_inline!(" **SafeCloset** ` ` $0 ` ` ", &path,);
+        let md = format!(" **SafeCloset** ` ` {} ` ` {} ", &path, state_info(state));
+        let composite = Composite::from_inline(&md);
         self.go_to_line(w, 0)?;
-        self.skin
-            .write_composite_fill(w, composite, self.width(), Alignment::Unspecified)?;
+        self.skin.write_composite_fill(w, composite, self.width(), Alignment::Unspecified)?;
         Ok(())
     }
 }
@@ -47,6 +47,46 @@ fn make_skin() -> MadSkin {
     let mut skin = MadSkin::default();
     skin.paragraph.set_fgbg(AnsiValue(252), AnsiValue(239));
     skin.bold.set_fg(ansi(222));
+    skin.italic = CompoundStyle::with_fg(AnsiValue(222));
     skin.inline_code.set_bg(gray(2));
     skin
 }
+
+fn state_info(state: &AppState) -> &'static str {
+    let deep = state.open_closet.depth() > 0;
+    match &state.drawer_state {
+        DrawerState::NoneOpen => {
+            if state.open_closet.just_created() && state.created_drawers == 0 {
+                "new closet"
+            } else {
+                "no open drawer"
+            }
+        }
+        DrawerState::DrawerCreation(_) => {
+            if deep {
+                "deep drawer creation"
+            } else {
+                "shallow drawer creation"
+            }
+        }
+        DrawerState::DrawerOpening(_) => {
+            if deep {
+                "deep drawer opening"
+            } else {
+                "shallow drawer opening"
+            }
+        }
+        DrawerState::DrawerEdit(des) => {
+            if des.touched() {
+                "*unsaved changes*"
+            // } else if deep {
+            //     "deep drawer"
+            // } else {
+            //     "shallow drawer"
+            } else {
+                ""
+            }
+        }
+    }
+}
+
